6080de8db81da92ef07c5fa649f451ddc6ec5e2f
Daniel Fieni Pereira <daniel_fieni@hotmail.com>
2023-01-07 18:19:37 -0300

    Referencing user posts

--- src/main/java/com/daniel/workshopMongo/domain/User.java
+++ src/main/java/com/daniel/workshopMongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.daniel.workshopMongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -13,6 +16,9 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy=true) // Allows the list to be loaded only when the user is instantiated
+	private List<Post> posts = new ArrayList<>(); // List of posts by a user
 	
 	public User() {
 	}
@@ -71,6 +77,14 @@ public class User implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 }
