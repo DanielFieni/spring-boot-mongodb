411d42ddbb0ee4a9cc2de00c7b884d0c9d95b0ac
Daniel Fieni Pereira <daniel_fieni@hotmail.com>
2023-01-07 22:50:30 -0300

    Getting a post by id

---
+++ src/main/java/com/daniel/workshopMongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.daniel.workshopMongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.daniel.workshopMongo.domain.Post;
+import com.daniel.workshopMongo.repository.PostRepository;
+import com.daniel.workshopMongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+	
+	// Find post by id
+	public Post findById(String id)
+	{
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+}
